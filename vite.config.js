e8dadbce87550bdec5f7571bbbc3c4a2b93e9200
AaronLiu615 <99209428+AaronLiu615@users.noreply.github.com>
2025-11-19 23:47:01 -0500

    fix images

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const base = process.env.VITE_BASE_URL || '/'
-
-  return {
-    plugins: [react()],
-    base,
-  }
+export default defineConfig({
+  plugins: [react()],
+  base: '/', 
 })
